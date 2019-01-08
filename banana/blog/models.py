9829fd262cf60f1ccad77e22073df594db5b6dbc
Haibo Yan <tristan.yim@gmail.com>
2019-01-08 00:02:20 -0800

    Add views

--- banana/blog/models.py
+++ banana/blog/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -10,6 +16,7 @@ class Post(models.Model):
         ('drafts', 'Drafts'),
         ('published', 'Published')
     )
+    published = PublishedManager()
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
@@ -25,3 +32,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.strftime('%m'),
+                                                 self.publish.strftime('%d'),
+                                                 self.slug])
+
