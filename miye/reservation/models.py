3f62825469eec4e2b759eba1decbb2509794d61c
Haibo Yan <tristan.yim@gmail.com>
2019-03-03 14:08:42 -0800

    Add reservation models

--- miye/reservation/models.py
+++ miye/reservation/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from administration.models import Customer
 from administration.models import Service
+import datetime
 
 
 # Create your models here.
@@ -8,3 +9,5 @@ class Reservation(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     reservation_date_time = models.DateTimeField
     reservation_service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    valid = all().filter(reservation_date_time > datetime.datetime.now())
+
--- miye/reservation/views.py
+++ miye/reservation/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from reservation.models import Reservation
+from administration.models import Customer
+from administration.models import Service
 
 # Create your views here.
 @login_required
@@ -47,9 +52,33 @@ class YearHtml(TemplateView):
 class YearMonthHtml(TemplateView):
     template_name = "calendar/year-month.html"
 
+
 @login_required
 def reservations(request):
-    return render(request, 'new_reservation.html', None)
+    data = dict()
+    try:
+        if request.method == 'GET':
+            valid_reservations = list(Reservation.valid.values())
+            data['reservation'] = valid_reservations
+        else:
+            customer = Customer.objects.filter(id == request.POST['customer_id'])
+            reservation_service = Service.objects.filter(id == request.POST['service_id'])
+            reservation_date_time = request.POST['reservation_time']
+            reservation = Reservation(customer=customer, reservation_date_time=reservation_date_time,
+                                  reservation_service=reservation_service)
+            reservation.save()
+
+            data['service'] = {
+                    'customer': customer,
+                    'reservation_date_time': reservation_date_time,
+                    'reservation_service': reservation_service
+                }
+        data['ret'] = 0
+    except ValidationError as e:
+        data['ret'] = 1
+        data['message'] = str(e)
+    return JsonResponse(data)
+
 
 @login_required
 def new_reservation(request):
