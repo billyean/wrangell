bbda13e3bca993250eb0474088459ca4680e5d01
Haibo Yan <tristan.yim@gmail.com>
2019-04-10 00:40:13 -0700

    Add application properties

--- jamaica/src/main/java/io/haibo/JamaicaApp.java
+++ jamaica/src/main/java/io/haibo/JamaicaApp.java
@@ -3,14 +3,14 @@ package io.haibo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@ComponentScan(basePackages = "io.haibo.controller")
 public class JamaicaApp {
     public static void main(String[] args) {
-        SpringApplication.run(JamaicaApp.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(JamaicaApp.class, args);
     }
 }
