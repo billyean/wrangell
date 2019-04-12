56edb705b789529c2d0c8832914192ee962a5ecb
Haibo Yan <tristan.yim@gmail.com>
2019-04-11 21:49:30 -0700

    Add h2-console

--- jamaica/src/main/java/io/haibo/controller/ServiceController.java
+++ jamaica/src/main/java/io/haibo/controller/ServiceController.java
@@ -6,11 +6,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class ServiceController {
     @GetMapping(value = "/service/{id}")
     public Service getServiceById(@PathVariable("id") int id) {
         return new Service(1, "Test", "Test", Arrays.asList(30, 60), 2.0, 1);
+    }
+
+    @GetMapping(value = "/services")
+    public List<Service> getServices() {
+        return Arrays.asList(
+                new Service(1, "Test", "Test", Arrays.asList(30, 60), 2.0, 1)
+        );
     }
 }
