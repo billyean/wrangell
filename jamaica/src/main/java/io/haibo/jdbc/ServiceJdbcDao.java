1a791d506c86c54dce1e704f39bf29e8b45b6cb0
Haibo Yan <tristan.yim@gmail.com>
2019-04-18 10:24:40 -0700

    Add custom RowMapper

--- jamaica/src/main/java/io/haibo/jdbc/ServiceJdbcDao.java
+++ jamaica/src/main/java/io/haibo/jdbc/ServiceJdbcDao.java
@@ -4,8 +4,11 @@ import io.haibo.model.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -13,14 +16,31 @@ public class ServiceJdbcDao {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    class ServiceRowMapper implements RowMapper<Service> {
+
+        @Override
+        public Service mapRow(ResultSet resultSet, int i) throws SQLException {
+            Service service = new Service();
+            service.setId(resultSet.getInt("id"));
+            service.setName(resultSet.getString("name"));
+            service.setDescription("general");
+            service.setTimeTypes(resultSet.getString("timeTypes"));
+            service.setRate(resultSet.getDouble("rate"));
+            service.setLimits(resultSet.getInt("limits"));
+            return service;
+        }
+    }
+
     public List<Service> findAll() {
-        return jdbcTemplate.query("SELECT * FROM service", new BeanPropertyRowMapper<Service>(Service.class));
+        //return jdbcTemplate.query("SELECT * FROM service", new BeanPropertyRowMapper<Service>(Service.class));
+        return jdbcTemplate.query("SELECT * FROM service", new ServiceRowMapper());
     }
 
     public Service findById(Integer id) {
         return jdbcTemplate.queryForObject("SELECT * FROM service Where id = ?",
                 new Object[]{id},
-                new BeanPropertyRowMapper<Service>(Service.class));
+//                new BeanPropertyRowMapper<Service>(Service.class));
+                new ServiceRowMapper());
     }
 
     public int deleteById(Integer id) {
