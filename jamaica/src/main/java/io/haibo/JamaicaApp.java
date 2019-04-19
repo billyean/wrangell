7a9b6e590f99a00e22009b588e401335c2803977
Haibo Yan <tristan.yim@gmail.com>
2019-04-18 23:22:35 -0700

    Fix runtime error

--- jamaica/src/main/java/io/haibo/JamaicaApp.java
+++ jamaica/src/main/java/io/haibo/JamaicaApp.java
@@ -1,24 +1,20 @@
 package io.haibo;
 
-import io.haibo.model.Service;
-import io.haibo.repositories.ServiceJpaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.List;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class JamaicaApp {
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass().getName());
 
-    @Autowired
-    ServiceJpaRepository repository;
+//    @Autowired
+//    ServiceJpaRepository repository;
 //    ServiceJdbcDao jdbcDao;
 
     public static void main(String[] args) {
