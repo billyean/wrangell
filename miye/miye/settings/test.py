c139141d6633030ea584805c1148ef0758df5e78
Haibo Yan <tristan.yim@gmail.com>
2019-04-25 23:16:33 -0700

    Fix allocation logic

--- miye/miye/settings/test.py
+++ miye/miye/settings/test.py
@@ -79,16 +79,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'miye.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -135,6 +125,6 @@ STATICFILES_DIRS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../test.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../test.sqlite3'),
     }
 }
